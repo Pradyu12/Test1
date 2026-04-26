92e1e1ee354ff8b04906bec5ab8daa539e9e589f
Pradyumna SK <115913218+Pradyu12@users.noreply.github.com>
2026-04-26 11:15:18 +0530

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controller/authController'); // Matches your 'controller' folder
-
-router.post('/login', authController.login);
-
-module.exports = router;
--- routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const orderController = require('../controllers/orderController');
-const auth = require('../middleware/auth');
-
-// Checkout: Process the order and clear the cart
-router.post('/checkout', auth, orderController.checkout);
-
-module.exports = router;
